1efca8a477ca42560210d8a8404542c90c64bf11
Denys Sych <denis.feelin@gmail.com>
2021-12-15 18:59:01 +0200

    just copypasted example code to see if it works wo errors

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,18 @@ import github from "@actions/github";
 import { getOctokitOptions } from "@actions/github/lib/utils";
 
 (async () => {
+
   try {
-    const token = core.getInput("repo-token");
-    const { owner, repo } = github.context.repo;
-    const prNumber = getPrNumber();
-    console.log(core, 'hello');
-    if (!prNumber) {
-      core.setFailed("Could not get pull request number from context");
-    }
-
-    const octokit = getOctokitOptions(token);
-
-    const response = await octokit.pulls.get({
-      owner: owner,
-      repo: repo,
-      pull_number: prNumber,
-    });
-    console.log({branchName: response.data.head.ref});
-
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput("who-to-greet");
+    const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
+    const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
   } catch (error: any) {
-    core.setFailed(error?.message);
+    core.setFailed(error.message);
   }
 })();
 
